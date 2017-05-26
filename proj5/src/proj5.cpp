3076fa47013732d64ce8392b8249ca7b063f1a38
migpir <tigerito123@yahoo.com>
2017-05-26 18:49:22 -0400

    Aniated line in OpenGL

--- proj5/src/proj5.cpp
+++ proj5/src/proj5.cpp
@@ -10,97 +10,53 @@
 // in c:\windows\SysWOW64  which is where any SANE person would expect.
 //============================================================================
 
-//#include <GL/glut.h>
-//#include <iostream>
-//#include <string>
-//#define WIDTH 800
-//#define HEIGHT 600
-
-#include <stdlib.h>
-#include <math.h>
+// Lesson 6: A Simple Animation
+// Author: Michael Hall
+//
+// This C++ code and project are provided "as is"
+// without warranty of any kind. For personal use only,
+// not for distribution. Copyright 2010 XoaX.
 #include <GL/glut.h>
 
-/* Sample func itself */
-float func(float x)
-{
-	return x*x*sin(x) - sin(x)*cos(x)/sin(x);
+GLfloat gfPosX = 0.0;
+GLfloat gfDeltaX = .01;
+
+void Draw() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_LINES);
+        glVertex3f(gfPosX, 0.25, 0.0);
+        glVertex3f(1.0 - gfPosX, 0.75,0.0);
+    glEnd();
+    glutSwapBuffers();
+    gfPosX += gfDeltaX;
+    if (gfPosX >= 1.0 || gfPosX <= 0.0) {
+        gfDeltaX = -gfDeltaX;
+    }
 }
 
-/* Function plotting func */
-void draw(float (* func)(float x), float x1, float x2, float y1, float y2, int N)
+void Timer(int iUnused)
 {
-	float x, dx = 1.0/N;
-
-	glPushMatrix(); /* GL_MODELVIEW is default */
-
-	glScalef(1.0 / (x2 - x1), 1.0 / (y2 - y1), 1.0);
-	glTranslatef(-x1, -y1, 0.0);
-	glColor3f(1.0, 1.0, 1.0);
-
-	glBegin(GL_LINE_STRIP);
-
-	for(x = x1; x < x2; x += dx)
-	{
-		glVertex2f(x, func(x));
-	}
-
-	glEnd();
-
-	glPopMatrix();
+    glutPostRedisplay();
+    glutTimerFunc(30, Timer, 0);
 }
 
-/* Redrawing func */
-void redraw(void)
-{
-	glClearColor(0, 0, 0, 0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	draw(func, -3, 3, 0, 10, 10);
-
-	glutSwapBuffers();
+void Initialize() {
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 
-/* Idle proc. Redisplays, if called. */
-void idle(void)
-{
-	glutPostRedisplay();
-}
-
-/* Key press processing */
-void key(unsigned char c, int x, int y)
-{
-	if(c == 27) exit(0);
-}
-
-/* Window reshape */
-void reshape(int w, int h)
-{
-	glViewport(0, 0, w, h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0, 1, 0, 1, -1, 1);
-	glMatrixMode(GL_MODELVIEW);
-}
-
-/* Main function */
-int main(int argc, char * argv[])
-{
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-	glutCreateWindow("Graph plotter");
-
-	/* Register GLUT callbacks. */
-	glutDisplayFunc(redraw);
-	glutKeyboardFunc(key);
-	glutReshapeFunc(reshape);
-	glutIdleFunc(idle);
-
-	/* Init the GL state */
-	glLineWidth(1.0);
-
-	/* Main loop */
-	glutMainLoop();
-	return 0;
+int main(int iArgc, char** cppArgv) {
+    glutInit(&iArgc, cppArgv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(250, 250);
+    glutInitWindowPosition(200, 200);
+    glutCreateWindow("XoaX.net");
+    Initialize();
+    glutDisplayFunc(Draw);
+    Timer(0);
+    glutMainLoop();
+    return 0;
 }
